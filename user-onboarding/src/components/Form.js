3ea10ed79b5b38bc48e8e8685756c96b467003d6
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-21 15:42:04 -0400

    added schema for formState

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -10,3 +10,19 @@ export default function Form() {
     password: "",
     terms: false,
   };
+
+  const [formState, setFormState] = useState(defaultState);
+  const [errors, setErrors] = useState({ ...defaultState, terms: "" });
+  const [disableButton, setDisabledButton] = useState(true);
+
+  // Form Schema for (formState)
+  let formSchema = yup.object().shape({
+    name: yup.string().required("Please provide name"),
+    email: yup
+      .string()
+      .required("Please provide an email")
+      .email("This is not a valid email."),
+    password: yup.string().required("Please enter a valid password").min(6),
+    terms: yup.boolean().oneOf([true], "Agree to Terms and Conditions"),
+  });
+}
